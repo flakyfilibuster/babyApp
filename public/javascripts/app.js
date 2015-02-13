2918cfcc0c409962eb6e034beeb7f2abeb81a657
flaky <flaky>
2015-02-13 21:47:32 +0100

    jQueried up

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,60 +1,48 @@
-var mealButton = $('#timerBtn .btn');
-var mealTable = document.getElementById('mealTableBody');
+var mealTable = $('#mealTableBody');
 
 // clickHandler for the mealButton
-mealButton.on('click', function(e) {
+$('#timerBtn .btn').on('click', function(e) {
   e.preventDefault();
 
   var self = $(this);
   self.prop("disabled", true);
 
   if (self.hasClass("mealStart")) {
-    ajax('get','/meals/start', null,
-      function success(data) {
-        var myRow = document.createElement("tr");
-        myRow.classList.add('current')
-        myRow.innerHTML = data;
-        mealTable.insertBefore(myRow, mealTable.firstChild);
-        self.toggleClass("btn-success mealStart btn-warning mealStop");
-        self.text('Stop Meal');
-        self.prop("disabled", false);
-      },
-      function error(err) {
-        console.log(err);
-        self.prop("disabled", false);
+    $.ajax({
+      type: "GET",
+      url: "/meals/start"
+    })
+    .error(function(err) {
+      console.log(err);
+      self.prop("disabled", false);
+    })
+    .done(function(data) {
+      // create tr element via $
+      var myRow = $(data).addClass("noEnd");
+
+      mealTable.find("tr:first").before(myRow);
+      self.toggleClass("btn-success mealStart btn-warning mealStop");
+      self.text('Stop Meal');
+      self.prop("disabled", false);
     });
   } else if (self.hasClass("mealStop")) {
-    ajax('get','/meals/stop', null,
-      function success(data) {
-        var myRow = document.createElement("tr");
-        myRow.innerHTML = data;
-        var replaceRow = mealTable.querySelector(".noEnd") || mealTable.querySelector(".current");
-        replaceRow.parentNode.replaceChild(myRow, replaceRow)
-        self.toggleClass("btn-success mealStart btn-warning mealStop");
-        self.text('Start Meal')
-        self.prop('disabled', false);
-      },
-      function error(err) {
-        console.log(err);
-        self.prop('disabled', false);
+    $.ajax({
+      type: "GET",
+      url: "/meals/stop"
+    })
+    .error(function(err) {
+      console.log(err);
+      self.prop('disabled', false);
+    })
+    .done(function(data) {
+      // create tr element via $
+      var myRow = $(data);
+
+      // find the row with no end time and replace it
+      mealTable.find(".noEnd").replaceWith(myRow);
+      self.toggleClass("btn-success mealStart btn-warning mealStop");
+      self.text('Start Meal');
+      self.prop('disabled', false);
     });
   }
 });
-
-
-// ajax utility function
-function ajax(method, url, params, cbSuccess, cbError) {
-  var xhr = new XMLHttpRequest()
-  xhr.open(method, url, true);
-  xhr.addEventListener('load', function(e) {
-    if(xhr.readyState === 4) {
-      if(xhr.status === 200) {
-        cbSuccess(xhr.response);
-      } else {
-        cbError(xhr.response);
-      }
-      console.log('done');
-    }
-  });
-  xhr.send(params);
-};
