8e128c679078d293d2523fdb38a2693e7134d4bc
flaky filibuster <flakyfilibuster@gmail.com>
2015-03-05 20:58:40 +0100

    Add routes for /pics

---
+++ routes/pics.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var db = require('../leveldb').connect();
+var picture = express.Router();
+var fs = require('fs');
+var path = require('path');
+
+picture
+.get('/', function (req, res) {
+  // get path for uploaded images
+  var dir = path.resolve(__dirname, '../public/uploads/') + '/';
+
+  // get the images and sort them by modified date
+  var pictures = fs.readdirSync(dir).sort(function(a, b) {
+    return fs.statSync(dir + a).mtime.getTime() - fs.statSync(dir + b).mtime.getTime();
+  });
+
+  res.render('pics', {
+    title: 'Line babyApp',
+    message: 'pictures',
+    data: pictures
+  });
+})
+
+.post('/', function(req, res) {
+  res.status(200).send('all ok');
+})
+
+module.exports = picture;
